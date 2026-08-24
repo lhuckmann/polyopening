78c0cf56fda3c2240f46d58b27115de4f2a96b7e
lhuckmann <l.huckmann@lic.leidenuniv.nl>
2026-08-24 18:05:05 -0500

    initial commit

---
+++ src/polyopening.py
@@ -0,0 +1,103 @@
+import numpy as np
+from ase import Atoms
+from ase.geometry import find_mic
+from ase.neighborlist import neighbor_list
+from grid.angular import AngularGrid
+
+
+class PolyhedralOpeningAngle:
+    """
+    Analyzer for polyhedral opening angles.
+
+    Notes
+    -----
+    The analyzer is stateless. Each call to ``compute()`` analyzes
+    one ASE ``Atoms`` object independently.
+    
+    Parameters
+    ----------
+    cutoff : float
+        Cutoff radius (Å) for identifying neighboring atoms.
+    lebedev_degree : int
+        Degree of the Lebedev angular grid.
+    atom_type : str, optional
+        Chemical symbol of the atoms for which the opening angle
+        should be evaluated. Default is "C".
+    """
+
+    def __init__(
+        self,
+        cutoff: float = 2.0,
+        lebedev_degree: int = 110,
+        atom_type: str = "all",
+    ) -> None:
+
+        self.cutoff = float(cutoff)
+        self.atom_type = atom_type
+        self.lebedev_degree = lebedev_degree
+
+        self.grid = AngularGrid(degree=lebedev_degree).points
+    
+    @staticmethod
+    def angle(rij, rik):
+        rad = np.arccos( ( np.dot( rij, rik ) ) / (np.sqrt(np.dot(rij, rij)) * np.sqrt(np.dot(rik, rik)) ) )
+        return np.rad2deg(rad)
+        
+    def _analyze_cp(
+            self,
+            atoms: Atoms,
+            refatom: np.ndarray,
+            i: np.ndarray,
+            j: np.ndarray,
+            refidx: int,
+            ) -> float:
+        neighbors = j[i == refidx]
+        if len(neighbors)==0:
+            print("\n*** WARNING ***\nempty neighborlist at atom "+str(refidx))
+            return np.nan
+        else:
+            neigh_coords=atoms.positions[neighbors]
+            local_grid=self.grid+refatom
+            dr = local_grid[:, None, :] - neigh_coords[None, :, :]
+            dr = dr.reshape(-1, 3)
+
+            dr_mic, lengths = find_mic(dr, atoms.cell, atoms.pbc)
+
+            lengths = lengths.reshape(len(self.grid), len(neigh_coords))
+        
+            # Distance to closest neighbor
+            nearest = np.min(lengths, axis=1)
+
+            # Best grid point
+            best_idx = np.argmax(nearest)
+        
+            closest_atom = np.argmin(lengths[best_idx])
+            xyz1,_=find_mic(local_grid[best_idx]-refatom, atoms.cell, atoms.pbc)
+            xyz2,_=find_mic(neigh_coords[closest_atom]-refatom, atoms.cell, atoms.pbc)
+            return self.angle(xyz1,xyz2)
+        
+        
+    def compute(
+            self,
+            atoms: Atoms,
+            ) -> list[float]:
+        elem=np.array(atoms.get_chemical_symbols())
+        coords=atoms.get_positions()
+        print(self.atom_type)
+        if "all" in self.atom_type:
+            xyz=np.copy(coords)
+        else:
+            idx=np.where(np.isin(elem, self.atom_type))[0]
+            xyz=coords[idx]
+        i, j = neighbor_list("ij", atoms, self.cutoff)
+        
+        PHI=[]
+        for refidx, refatom in enumerate(xyz):
+            phi=self._analyze_cp(atoms,refatom,i,j,refidx)
+            PHI.append(phi)
+        return np.array(PHI)
+    
+
+            
+            
+            
